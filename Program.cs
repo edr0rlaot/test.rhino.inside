14cede4858d0fb34abe58d3d51d8f1deb72f1755
Steve Baer <steve@mcneel.com>
2022-04-15 11:35:37 -0700

    Remove unnecessary references and add static Resolver call

--- Program.cs
+++ Program.cs
@@ -6,25 +6,28 @@ namespace TestRhinoInside
 {
   class Program
   {
+    static Program()
+    {
+      RhinoInside.Resolver.Initialize();
+    }
+
+
     static string OpenRhinoFile(string path)
     {
       try
       {
-        using (new RhinoCore(new string[] { "/NOSPLASH" }, WindowStyle.NoWindow))
+        var doc = RhinoDoc.Open(path, out bool wasAlreadyOpen);
+        if (doc == null)
         {
-          var doc = RhinoDoc.Open(path, out bool wasAlreadyOpen);
-          if (doc == null)
-          {
-            Console.WriteLine("error, failed to load file");
-            return "failed...";
-          }
-          else
-          {
-            Console.WriteLine("file loaded successfully");
-            System.Threading.Thread.Sleep(2000);
-            doc.Dispose();
-            return "done...";
-          }
+          Console.WriteLine("error, failed to load file");
+          return "failed...";
+        }
+        else
+        {
+          Console.WriteLine("file loaded successfully");
+          System.Threading.Thread.Sleep(2000);
+          doc.Dispose();
+          return "done...";
         }
       }
       catch (Exception ex)
@@ -35,16 +38,31 @@ namespace TestRhinoInside
 
     static void Main(string[] args)
     {
-      if (args.Length < 1)
+      using (new RhinoCore())
       {
-        Console.WriteLine("please, select a .3dm file...");
-      }
-      else
-      {
-        var path = args[0];
-        Console.WriteLine("input file : {0}", path);
-        var result = OpenRhinoFile(path);
-        Console.WriteLine(result);
+        //if (args.Length < 1)
+        //{
+        //  Console.WriteLine("please, select a .3dm file...");
+        //}
+        //else
+        //{
+        //  var path = args[0];
+        //  Console.WriteLine("input file : {0}", path);
+        //  var result = OpenRhinoFile(path);
+        //  Console.WriteLine(result);
+        //}
+
+        string[] paths = new string[] {@"C:\dev\github\sbaer\archi.3dm",
+      @"C:\dev\github\sbaer\simple-shape.3dm",
+      @"C:\dev\github\sbaer\City_NURBS.3dm" };
+
+        foreach (var path in paths)
+        {
+          Console.WriteLine("input file : {0}", path);
+          var result = OpenRhinoFile(path);
+          Console.WriteLine(result);
+        }
+
       }
     }
   }
